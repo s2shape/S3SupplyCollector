0ac6a6901730c1512ddf0b4c9e01de164f9fea11
Mikhail Burilov <burilovmv@gmail.com>
2019-08-20 10:42:15 +0700

    Fixed s3 tests, implemented connect string additions

--- S3SupplyCollector/S3SupplyCollector.cs
+++ S3SupplyCollector/S3SupplyCollector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
@@ -13,19 +14,38 @@ namespace S3SupplyCollector
     public class S3SupplyCollector : DriveSupplyCollectorBase.DriveSupplyCollectorBase {
         private string _bucketName;
 
+        private const string PREFIX = "s3://";
+
+        public S3SupplyCollector(string s2Prefix = null, int s2FolderLevels = 0, bool s2UseFileNameInDcName = false) : base(s2Prefix, s2FolderLevels, s2UseFileNameInDcName)
+        {
+        }
+
         public override List<string> DataStoreTypes() {
             return (new[] { "S3" }).ToList();
         }
 
         public string BuildConnectionString(string accessKey, string secretKey, string region, string bucket) {
-            return $"s3://{accessKey}:{secretKey}@{region}/{bucket}";
+            var attrs = new StringBuilder();
+            if (s2Prefix != null)
+            {
+                attrs.Append($",s2-prefix={s2Prefix};");
+            }
+            if (s2FolderLevels != 0)
+            {
+                attrs.Append($",s2-folder-levels-used-in-dc-name={s2FolderLevels};");
+            }
+            if (s2UseFileNameInDcName)
+            {
+                attrs.Append(",s2-use-file-name-in-dc-name=True;");
+            }
+            return $"{PREFIX}{accessKey}:{secretKey}@{region}/{bucket}{attrs}";
         }
 
         private AmazonS3Client Connect(string connectString) {
-            if(!connectString.StartsWith("s3://"))
+            if(!connectString.StartsWith(PREFIX))
                 throw new ArgumentException("Invalid connection string!");
 
-            var keyIndex = "s3://".Length;
+            var keyIndex = PREFIX.Length;
             var secretIndex = connectString.IndexOf(":", keyIndex);
             if (secretIndex <= 0)
                 throw new ArgumentException("Invalid connection string!");
@@ -35,11 +55,19 @@ namespace S3SupplyCollector
             var bucketIndex = connectString.IndexOf("/", regionIndex);
             if (bucketIndex <= 0)
                 throw new ArgumentException("Invalid connection string!");
+            var additionsIndex = connectString.IndexOf(",", bucketIndex);
 
             var accessKey = connectString.Substring(keyIndex, secretIndex - keyIndex);
             var secretKey = connectString.Substring(secretIndex + 1, regionIndex - secretIndex - 1);
             var region = connectString.Substring(regionIndex + 1, bucketIndex - regionIndex - 1);
-            _bucketName = connectString.Substring(bucketIndex + 1);
+
+            if (additionsIndex > 0) {
+                _bucketName = connectString.Substring(bucketIndex + 1, additionsIndex - bucketIndex - 1);
+                ParseConnectionStringAdditions(connectString.Substring(additionsIndex + 1));
+            }
+            else {
+                _bucketName = connectString.Substring(bucketIndex + 1);
+            }
 
             return new AmazonS3Client(accessKey, secretKey, RegionEndpoint.GetBySystemName(region));
         }
--- S3SupplyCollectorTests/S3SupplyCollectorTests.cs
+++ S3SupplyCollectorTests/S3SupplyCollectorTests.cs
@@ -4,17 +4,24 @@ using Xunit;
 
 namespace S3SupplyCollectorTests
 {
-    public class S3SupplyCollectorTests
+    public class S3SupplyCollectorTests : IClassFixture<LaunchSettingsFixture>
     {
         private readonly S3SupplyCollector.S3SupplyCollector _instance;
         public readonly DataContainer _container;
+        private LaunchSettingsFixture _fixture;
 
-        public S3SupplyCollectorTests()
+        public S3SupplyCollectorTests(LaunchSettingsFixture fixture)
         {
+            _fixture = fixture;
             _instance = new S3SupplyCollector.S3SupplyCollector();
             _container = new DataContainer()
             {
-                ConnectionString = _instance.BuildConnectionString("", "", "us-east-1", "itsplus")
+                ConnectionString = _instance.BuildConnectionString(
+                    Environment.GetEnvironmentVariable("S3_ACCESS_KEY"),
+                    Environment.GetEnvironmentVariable("S3_SECREY_KEY"),
+                    Environment.GetEnvironmentVariable("S3_REGION"),
+                    Environment.GetEnvironmentVariable("S3_CONTAINER")
+                    )
             };
         }
 
@@ -49,7 +56,33 @@ namespace S3SupplyCollectorTests
             var samples = _instance.CollectSample(entity, 5);
             Assert.Equal(5, samples.Count);
             Assert.Contains("sally@example.com", samples);
+        }
 
+        [Fact]
+        public void TestFilenamesInSchema()
+        {
+            var prefixCollector = new S3SupplyCollector.S3SupplyCollector("emails/2019/08", 0, true);
+            var (tables, elements) = prefixCollector.GetSchema(_container);
+
+            Assert.Equal(1, tables.Count);
+            Assert.Equal(39, elements.Count);
+            Assert.Equal("EMAILS-UTF8.CSV", tables[0].Name);
+
+            var levelsCollector = new S3SupplyCollector.S3SupplyCollector(null, 1, false);
+            (tables, elements) = levelsCollector.GetSchema(_container);
+
+            Assert.Equal(1, tables.Count);
+            Assert.Equal(39, elements.Count);
+            Assert.Equal("emails", tables[0].Name);
+
+            var noprefixCollector = new S3SupplyCollector.S3SupplyCollector(null, 1, true);
+            (tables, elements) = noprefixCollector.GetSchema(_container);
+
+            Assert.Equal(2, tables.Count);
+            Assert.Equal(69, elements.Count);
+
+            Assert.NotNull(tables.Find(x => x.Name.Equals("emails/EMAILS-UTF8.CSV")));
+            Assert.NotNull(tables.Find(x => x.Name.Equals("emails/emails-utf8.parquet")));
         }
 
     }
